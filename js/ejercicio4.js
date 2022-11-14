abb12d544aea4e9a2d29ee6409bb7976dd396541
Sonia Moreno <soniamp95@gmail.com>
2022-11-14 17:41:42 +0100

    ejercicio4 js

---
+++ js/ejercicio4.js
@@ -0,0 +1,19 @@
+var cadena1 = "cadena1";
+var cadena2;
+
+console.log(typeOF(cadena1));
+console.log(typeOF(cadena2));
+
+if(typeOf(cadena2) == undefined){
+    console.log("SIN DEFINIR");
+}
+
+var cliente ={
+    nombre: "PEPITO",
+    tel: "124665525"
+};
+
+cliente.direccion = "C/ Salud21";
+
+console.log(cliente);
+
