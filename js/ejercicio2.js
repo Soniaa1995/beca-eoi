afe245cb2e2c80e6b6cc17ce3d756fa2e5b786f6
Sonia Moreno <soniamp95@gmail.com>
2022-11-14 16:19:02 +0100

    js 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,6 @@
+var alto = 12;
+var ancho = 12.203004;
+var altoPorAncho = alto * ancho;
+console.log("El resultado de multiplicar " + alto + " y " + ancho + " es = " + altoPorAncho);
+console.log(altoPorAncho.toFixed(2));
+
