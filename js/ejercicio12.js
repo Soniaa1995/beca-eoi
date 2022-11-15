0de4972c4ef6ddc6166aeab69c51417a271c7f54
Sonia Moreno <soniamp95@gmail.com>
2022-11-15 19:25:04 +0100

    ej12js

---
+++ js/ejercicio12.js
@@ -0,0 +1,11 @@
+let nombre = "Sonia";
+var obj = {
+    nombre: "Pepito",
+        saludo: function(){
+            var saludo_fn = function(){
+                console.log("hola " + this.obj.nombre);
+            };
+            saludo_fn();
+        } 
+};
+obj.saludo();
