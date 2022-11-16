47e62f9c8a35525be0eff24c859b0f78875fe0e8
Sonia Moreno <soniamp95@gmail.com>
2022-11-16 17:45:47 +0100

    ej18 js

---
+++ js/ejercicio18.js
@@ -0,0 +1,9 @@
+var header = document.getElementsByTagName("header");
+console.log(header);
+
+var header2 = document.getElementById("cabecera")
+console.log(header2);
+
+var header3 = document.querySelector("header")
+console.log(header3);
+
