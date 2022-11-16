b9e495f7e6aaf1c1da17e66c4145ab1f4dc2502c
Sonia Moreno <soniamp95@gmail.com>
2022-11-16 16:22:17 +0100

    correcion9

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -6,14 +6,14 @@ function suma(num1, num2){
 
 //c) Define una funcion que reciba dos parametros y devuelva el resultado de una multiplicacion
 function multiplica(num1, num2){
-    return rnum1*num2;
+    return num1*num2;
 }
 
 function operacion(){
-    let numero1 = Number(document.getElementById('numero1').innerHTML);
-    let numero2 = Number(document.getElementById('numero2').innerHTML);
+    let numero1 = Number(document.getElementById("numero1").textContent);
+    let numero2 = Number(document.getElementById("numero2").textContent);
     let sum = suma(numero1, numero2);
     let multiplicacion = multiplica(numero1, numero2);
-    suma(sum, multi);
+    suma(sum, multiplicacion);
+    console.log(sum + " - " + multiplicacion);
 }
-operacion();
